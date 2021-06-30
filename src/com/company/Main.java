a44e3747f65740c43637575b73bed707f0b59e1d
romankabaev <romakabaev2222@gmail.com>
2021-06-30 21:42:20 +0300

    Client

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,60 @@
 package com.company;
 
-public class Main {
+import javax.sound.sampled.LineUnavailableException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main(String[] args) throws LineUnavailableException, SocketException, UnknownHostException {
+
+        Thread receiver = new Thread() {
+            public void run() {
+                SpeakersWriter speakers = null;
+                try {
+                    speakers = new SpeakersWriter();
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+                PlayerThread playerThread = null;
+                try {
+                    playerThread = new PlayerThread(speakers.start());
+                } catch (SocketException | LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+                playerThread.start();
+                Scanner sc = new Scanner(System.in);
+                sc.nextLine();
+                playerThread.close();
+                speakers.close();
+            }
+        };
+
+
+        Thread sender = new Thread() {
+            public void run() {
+                MicrophoneReader microphoneReader = null;
+                try {
+                    microphoneReader = new MicrophoneReader();
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+                RecorderThread recorderThread = null;
+                try {
+                    recorderThread = new RecorderThread(microphoneReader.start());
+                } catch (SocketException | LineUnavailableException | UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                recorderThread.start();
+                Scanner sc = new Scanner(System.in);
+                sc.nextLine();
+                recorderThread.close();
+                microphoneReader.close();
+            }
+        };
+        receiver.start();
+        sender.start();
+
+
     }
 }
